3bde35bf5acca2b2c9f9c8792ea8273804e8e2e5
Inayaili de Leon <inayaili@gmail.com>
2018-03-11 19:02:08 +0000

    Tweaks footer, exhibitions, home, about

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,14 @@ toggle.addEventListener("click", function(){
     container.style.display = 'none';
   }
 }, false);
+
+var toggle = document.getElementById("toggle-previous-exhibitions-2"),
+    container = document.getElementById("previous-exhibitions");
+
+toggle.addEventListener("click", function(){
+  if (container.style.display == 'none') {
+    container.style.display = 'grid';
+  } else {
+    container.style.display = 'none';
+  }
+}, false);
